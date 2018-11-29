70586c56339356cb32c729a0c535a8440f8c3f39
Yash Shah <yashrshah14@gmail.com>
2018-11-29 14:12:32 -0500

    Add files via upload
    Knuth-Morris-Pratt pattern matching algorithm implemented in Python.

---
+++ kmp.py
@@ -0,0 +1,41 @@
+def computeLPS(p,p_len,lps):
+    length=0
+    i=1
+    while(i<p_len):
+        if p[i]==p[length]:
+            length+1
+            lps[i]=length
+            i+=1
+        else:
+            if length!=0:
+                length=lps[length-1]
+            else:
+                lps[i]=0
+                i+=1
+
+
+s=raw_input("Enter the string")
+p=raw_input("Enter the pattern")
+s_len=len(s)
+p_len=len(p)
+lps=[0]*p_len
+j=0
+computeLPS(p,p_len,lps)
+i=0
+while i<s_len:
+    if p[j]==s[i]:
+        i+=1
+        j+=1
+    if j==p_len:
+        print("Found pattern at index:",str(i-j))
+        j=lps[j-1]
+    elif(i<s_len and p[j]!=s[i]):
+         if (j!=0):
+             j=lps[j-1]
+         else:
+             i+=1
+         
+        
+            
+            
+    
